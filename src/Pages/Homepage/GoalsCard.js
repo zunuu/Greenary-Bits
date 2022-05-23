5c62db2ea0ad04a183ad1398ed1bf5cb72949df6
zunuu <zunayedhossain99@gmail.com>
2022-05-23 21:01:00 +0600

    magic magic magic

--- src/Pages/Homepage/GoalsCard.js
+++ src/Pages/Homepage/GoalsCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import './GoalsCard.css'
 const GoalsCard = () => {
     return (
-        <div className='bg-slate-300 -mb-28'>
-            <div className='mask mask-decagon p-28 bg-neutral'>
+        <div className='bg-slate-300'>
+            <div className='bg-transperent max-w-fit justify-center mx-auto -mb-28 goalsCard'>
+                <div className='mask mask-decagon p-28 bg-neutral'>
 
+
+                </div>
 
             </div>
-
         </div>
     );
 };
